1942ada38cd2bf6b3683f4248087bf80c013a3ba
Jonathan <jonathan_hrg@hotmail.com>
2020-10-07 13:25:05 -0500

    Add actions for adding and deleting

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
-export const Transaction = ({transaction}) => {
-    const sign = transaction.amount < 0 ? '-' : '+'
-    const style = transaction.amount < 0 ? 'minus' : 'plus'
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const style = transaction.amount < 0 ? 'minus' : 'plus';
   return (
     <li className={style}>
-      {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-      <button className="delete-btn">x</button>
+      {transaction.text}{' '}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => {
+          deleteTransaction(transaction.id);
+        }}
+      >
+        x
+      </button>
     </li>
   );
 };
